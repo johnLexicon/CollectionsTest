7a13cbb4462bf707beaaa7198e64e2940388617b
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-12 09:52:35 +0100

    First commit

---
+++ CollectionsTest.Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsTest.Tests
+{
+    [TestFixture]
+    public class ArrayTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail();
+        }
+    }
+}
